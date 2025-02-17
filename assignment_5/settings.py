33b672a5617dab30d9d5c636750862d497375058
niamhfoley-dev <niamhfoley.dev@gmail.com>
2025-02-17 23:22:18 +0000

    use whitenoise for static

--- assignment_5/settings.py
+++ assignment_5/settings.py
@@ -119,6 +119,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
