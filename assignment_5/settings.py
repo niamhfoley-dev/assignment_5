edba58642ba66858ae19e0429ef5f41788141358
niamhfoley-dev <niamhfoley.dev@gmail.com>
2025-02-17 23:28:56 +0000

    fix static

--- assignment_5/settings.py
+++ assignment_5/settings.py
@@ -114,15 +114,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Global static files
-    # Add other directories if needed
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
